c105337a75a2f91982f24c6cf95e3126748ee096
liyili <732193462@qq.com>
2019-07-08 15:39:58 +0800

    feat:add sectionlist

--- App.js
+++ App.js
@@ -5,27 +5,47 @@
  * @format
  * @flow
  */
-import React,{Component} from 'react';
-import{AppRegistry,ScrollView,Image,Text} from 'react-native';
+import React, { Component } from 'react';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
-export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component{
-  render(){
-    return(
-      <ScrollView>
-        <Text style={{fontSize:96}}>Scroll me plz</Text>
-        <Image source={{uri:'https://facebook.github.io/react-native/img/favicon.png',width:64,height:64}} />
-        <Image source={{uri:'https://facebook.github.io/react-native/img/favicon.png',width:64,height:64}} />
-        <Image source={{uri:'https://facebook.github.io/react-native/img/favicon.png',width:64,height:64}} />
-        <Text style={{fontSize:96}}>If you like</Text>
-        <Image source={{uri:'https://facebook.github.io/react-native/img/favicon.png',width:64,height:64}} />
-        <Image source={{uri:'https://facebook.github.io/react-native/img/favicon.png',width:64,height:64}} />
-        <Image source={{uri:'https://facebook.github.io/react-native/img/favicon.png',width:64,height:64}} />
-        <Image source={{uri:'https://facebook.github.io/react-native/img/favicon.png',width:64,height:64}} />
-        <Text style={{fontSize:96}}>And your findings</Text>  
-      </ScrollView>
-    )
+export default class SectionListBasics extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
 // import React, {Fragment} from 'react';
 // import {
 //   SafeAreaView,
